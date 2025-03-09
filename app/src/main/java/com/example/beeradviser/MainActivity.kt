c27aad8bf87434d40e89cc2157901ec414cbcf19
Anton Belyaev <belyash23@inbox.ru>
2025-03-09 20:19:57 +0400

    добавил фильтр по вкусу

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
+import org.json.JSONArray
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +17,43 @@ class MainActivity : AppCompatActivity() {
         findBeer.setOnClickListener {
             val beerColor = findViewById<Spinner>(R.id.beer_color)
             val color = beerColor.selectedItem
-            val beerList = getBeers(color.toString())
+
+            val beerTaste = findViewById<Spinner>(R.id.beer_taste)
+            val taste = beerTaste.selectedItem
+
+            val beerList = getBeers(color.toString().lowercase(), taste.toString().lowercase())
             val beers = beerList.reduce { str, item -> str + '\n' + item }
             val brands = findViewById<TextView>(R.id.brands)
             brands.text = beers
         }
     }
 
-    fun getBeers(color: String): List<String> {
-        return when (color) {
-            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
-            "Amber" -> listOf("Jack Amber", "Red Moose")
-            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
-            else -> listOf("Gout Stout", "Dark Daniel")
+    fun getBeers(color: String, taste: String): List<String> {
+        val beersData: JSONObject = getBeersData();
+
+        for (beersDataColor in beersData.keys()) {
+            val beersDataTastes: JSONObject = beersData.get(beersDataColor) as JSONObject
+            for (beersDataTaste in beersDataTastes.keys()) {
+                if (beersDataColor == color
+                    && beersDataTaste == taste
+                    && beersDataTastes.get(beersDataTaste) is JSONArray) {
+
+                    val beers = beersDataTastes.get(beersDataTaste) as JSONArray
+                    val list = mutableListOf<String>()
+                    for (i in 0 until beers.length()) {
+                        val beer: String = beers[i] as String
+                        list.add(beer)
+                    }
+                    return list
+                }
+            }
         }
+
+        return listOf()
+    }
+
+    fun getBeersData(): JSONObject {
+        val fileContent: String = applicationContext.assets.open("beers.json").bufferedReader().use { it.readText() }
+        return JSONObject(fileContent)
     }
 }
