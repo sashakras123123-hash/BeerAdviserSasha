15127afa2b29528de0ad6a315b69ed1ee805a11a
sashakras123123-hash <sashakras123123@gmail.com>
2025-09-18 13:41:28 +0400

    Update MainActivity.kt

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -1,35 +1,116 @@
-package com.example.beeradviser
+package com.example.stopwatch
 
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Chronometer
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stopwatch: Chronometer
+    lateinit var imageView: ImageView
+    var running = false
+    var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val findBeer = findViewById<Button>(R.id.find_beer)
-        findBeer.setOnClickListener {
-            val beerColor = findViewById<Spinner>(R.id.beer_color)
-            val color = beerColor.selectedItem
-            val beerList = getBeers(color.toString())
-            val beers = beerList.reduce { str, item -> str + '\n' + item }
-            val brands = findViewById<TextView>(R.id.brands)
-            brands.text = beers
+
+        stopwatch = findViewById(R.id.stopwatch)
+        imageView = findViewById<ImageView>(R.id.imageView2)
+        imageView.setImageResource(R.drawable.f9ccb485192f444c9bf840360cef6)// Ищем картинку по ID
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+                startRotation()
+            } else setBaseTime()
+        }
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                startRotation()
+                running = true
+            }
+        }
+
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                stopRotation()
+                running = false
+            }
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
         }
     }
 
-    fun getBeers(color: String): List<String> {
-        return when (color) {
-            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
-            "Amber" -> listOf("Jack Amber", "Red Moose")
-            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
-            else -> listOf("Gout Stout", "Dark Daniel")
+    override fun onPause() {
+        super.onPause()
+        if (running) {
+            saveOffset()
+            stopwatch.stop()
+            stopRotation()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            stopwatch.start()
+            startRotation()
+            offset = 0
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(outState)
+    }
+
+    fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+    private fun startRotation() {
+        val rotate = RotateAnimation(
+            0f, 360f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        rotate.duration = 1000 // Время одного полного оборота
+        rotate.repeatCount = Animation.INFINITE // Бесконечное повторение
+        imageView.startAnimation(rotate)
+    }
+
+    private fun stopRotation() {
+        imageView.clearAnimation()
     }
 }
